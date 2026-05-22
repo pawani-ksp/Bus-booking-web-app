82106819d4b08aba5d04b341e46b1f7147a5b163
pawani-ksp <2023cs222@stu.ucsc.cmb.ac.lk>
2026-05-22 19:07:38 +0530

    landing page

---
+++ frontend/.angular/cache/21.2.12/bus-booking-system/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-QX73SHOS.js";
+import "./chunk-WC4PITUE.js";
+import "./chunk-LTXVKK7M.js";
+import "./chunk-HQFU2FRM.js";
+import "./chunk-IRRBUIUN.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
---
+++ frontend/src/app/pages/landing/landing.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  imports: [RouterLink],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css',
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Landing {
+export class Login {
+}
+}
